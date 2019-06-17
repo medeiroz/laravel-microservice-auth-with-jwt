67a2054583e5ce39b088bb0abd10cfb36b16ac15
Flavio Medeiros <flavio.medeiros@fire-engine-red.com>
2019-06-17 16:45:44 -0300

    Alterado a importação

--- app/Traits/Treat.php
+++ app/Traits/Treat.php
@@ -4,16 +4,19 @@ namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use App\Http\RequestRestFulFilter;
+use App\Http\RequestRestFulFilter\Select;
+use App\Http\RequestRestFulFilter\Where;
+use App\Http\RequestRestFulFilter\OrderBy;
+use App\Http\RequestRestFulFilter\Paginate;
 
 trait Treat
 {
     public function scopeTreat(Builder $builder, Request $request)
     {
-        $select = new RequestRestFulFilter\Select($builder, $request);
-        $where = new RequestRestFulFilter\Where($builder, $request);
-        $orderBy = new RequestRestFulFilter\OrderBy($builder, $request);
-        $paginate = new RequestRestFulFilter\Paginate($builder, $request);
+        $select = new Select($builder, $request);
+        $where = new Where($builder, $request);
+        $orderBy = new OrderBy($builder, $request);
+        $paginate = new Paginate($builder, $request);
 
         $select->apply();
         $where->apply();
